71bb97b1b43eaff14ebdefae5e244d4f267f0227
Daniel James Dolor <estheryahe@gmail.com>
2021-09-04 16:54:19 +0800

    added viewing post functionality

--- myblogapp/urls.py
+++ myblogapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_feed, name='blog_newsfeed'),
+    path('post/<int:pk>/', views.view_blogpost, name='viewblog'),
 ]
--- myblogapp/views.py
+++ myblogapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import BlogPost
 
@@ -7,3 +7,6 @@ def blog_feed(request):
     blogposts = BlogPost.objects.filter(published_date__lte=timezone.now())
     return render(request, 'myblogapp/newsfeed.html',{'bps':blogposts})
 
+def view_blogpost(request, pk):
+    blogpost = get_object_or_404(BlogPost, pk=pk)
+    return render(request, 'myblogapp/blogdetail.html',{'p':blogpost})
