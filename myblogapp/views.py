e208874554e87f3ccdac8cebc66e4bc518c6b79b
Daniel James Dolor <estheryahe@gmail.com>
2021-09-04 16:04:01 +0800

    I'm at CSS part alr

--- myblogapp/views.py
+++ myblogapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import BlogPost
 
 # Create your views here.
+def blog_feed(request):
+    blogposts = BlogPost.objects.filter(published_date__lte=timezone.now())
+    return render(request, 'myblogapp/newsfeed.html',{'bps':blogposts})
+
