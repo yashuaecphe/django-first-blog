31b6e3973f6b54a4120d9c87fc8d476b15140866
Daniel James Dolor <yahshua.ecphe@gmail.com>
2021-09-09 15:33:10 +0800

    did the postgres setup and migrations

--- myblogsite/settings.py
+++ myblogsite/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'myblogsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER':'name',
-        'PASSWORD':'',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER':'djdols',
+        'PASSWORD':'djdols',
         'HOST':'localhost',
-        'PORT':'',
+        #'PORT':'',
     }
 }
 
