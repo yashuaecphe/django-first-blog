cb877bc059c93120dcaff326a7e7326a5196d927
Daniel James Dolor <yahshua.ecphe@gmail.com>
2021-09-22 14:54:17 +0800

    Modified BlogList API that if GET, if authorized,will return list of all posts from the user, if not authorized, will just return published posts. For POST, it needs authentication now

--- api/view/blogpost.py
+++ api/view/blogpost.py
@@ -3,29 +3,44 @@ from api.serializers import BlogpostSerializer
 from django.http import Http404
 from django.utils import timezone
 from myblogapp.models import BlogPost
-
-from rest_framework.decorators import permission_classes, authentication_classes
-from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import permissions
+
 
 class BlogList(APIView): 
     """ api/blogposts/
-        - GET list of all published blogposts
+        - GET list of
+            if authenticated: list of all blogposts by the user (even unpublished)
+            else: list of all published blogposts
         - POST a new blogpost (REQUIRES AUTH)
     """
+    #permission_classes = [permissions.IsAuthenticated]
     def get(self, request, format=None):
-        blogposts = BlogPost.objects.all().filter(published_date__lte=timezone.now())
-        serializer = BlogpostSerializer(blogposts, many=True)
-        return Response(serializer.data)
+        if request.user.is_authenticated:
+            # if authenticated, will return all the posts of the user
+            blogposts = BlogPost.objects.all().filter(author=request.user.id)
+            serializer = BlogpostSerializer(blogposts, many=True)
+            return Response(serializer.data)
+        else:
+            # if not, will return all published posts
+            blogposts = BlogPost.objects.all().filter(published_date__lte=timezone.now())
+            serializer = BlogpostSerializer(blogposts, many=True)
+            return Response(serializer.data)
+        
 
     def post(self, request, format=None):
-        serializer = BlogpostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if request.user.is_authenticated:
+            serializer = BlogpostSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"detail":"Authentication required"}, status.HTTP_401_UNAUTHORIZED)
+
+
     
 
 class BlogDetail(APIView):
