b49245a3b632bb78ab5c676bafc7542374b2fa82
Daniel James Dolor <yahshua.ecphe@gmail.com>
2021-09-22 17:19:37 +0800

    Added authoriation conditions to my blog endpoints. Like, only the author can update and delete blogposts, and only the author can view his unpublished blogposts

--- api/view/blogpost.py
+++ api/view/blogpost.py
@@ -1,6 +1,6 @@
 
 from api.serializers import BlogpostSerializer
-from django.http import Http404
+from django.http import Http404, Http403
 from django.utils import timezone
 from myblogapp.models import BlogPost
 from rest_framework.views import APIView
@@ -55,16 +55,15 @@ class BlogDetail(APIView):
             requires authentication and that the user should be the author of that post
     """
     def get_object(self,pk)->BlogPost:
-        try:
-            return BlogPost.objects.get(pk=pk)
-        except BlogPost.DoesNotExist:
-            raise Http404
+        try: return BlogPost.objects.get(pk=pk)
+        except BlogPost.DoesNotExist: raise Http404
     
     def get(self, request, pk, format=None):
         blogpost = self.get_object(pk)
         serializer = BlogpostSerializer(blogpost)
         if (blogpost.published_date is None) and (request.user!=blogpost.author): #post is unpublished
-            return Response(data={"detail":"You are not authorized to view this unpublished post"},status=status.HTTP_403_FORBIDDEN)
+            #return Response(data={"detail":"You are not authorized to view this unpublished post"},status=status.HTTP_403_FORBIDDEN)
+            raise Http403
         return Response(serializer.data)
 
     def put(self, request, pk, format=None):
